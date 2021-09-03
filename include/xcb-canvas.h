60a07448492d98bd63e574e4ef1f84860cd78b9c
Tudi20 <tudorkae@gmail.com>
2021-09-03 13:50:57 +0200

    Update file structure to be more library-like

--- include/xcb-canvas.h
+++ include/xcb-canvas.h
@@ -6,11 +6,23 @@
 
 #include <xcb/xcb.h>
 
+typedef struct RenderingContext {
+    int width;
+    int height;
+    xcb_window_t win;
+    xcb_connection_t *c;
+} RenderingContext;
+
+RenderingContext rendering_context;
+
 /* Prints pressed modifier keys */
 void print_modifiers (uint32_t mask);
 
 /* Program entry-point, when executed */
 int init_xcb ();
+
+/* Get/Set the RenderingContext's properties */
+void set_window_size (RenderingContext* rendering_context, int new_width, int new_height);
 
 /* Handle the event loop of the main program */
 void handle_events (xcb_connection_t *c);
