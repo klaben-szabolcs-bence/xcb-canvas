0f9485d16779713b9a76244fefb802f0c7d09261
Tudi20 <tudorkae@gmail.com>
2021-11-29 03:54:17 +0100

    Remove non-canvas drawing primitives

--- include/canvas.h
+++ include/canvas.h
@@ -11,29 +11,10 @@
 */
 void canvas_init(canvas_rendering_context_t* rendering_context);
 
-/** 
- * @brief Draws a point.
- * 
- * @param x                 The x coordinate of the point.
- * @param y                 The y coordinate of the point.
- * @param color             The color of the point.
- * @param rendering_context The rendering context to use.
-*/
-void canvas_draw_point (canvas_rendering_context_t* rendering_context, int16_t x, int16_t y);
+/* TODO: Add "path" support from Canvas. */
 
 /** 
- * @brief Draws a line.
- * 
- * @param rendering_context The rendering context.
- * @param x1                The x coordinate of the first point.
- * @param y1                The y coordinate of the first point.
- * @param x2                The x coordinate of the second point.
- * @param y2                The y coordinate of the second point.
-*/
-void canvas_draw_line(canvas_rendering_context_t* rendering_context, int16_t x1, int16_t y1, int16_t x2, int16_t y2);
-
-/** 
- * @brief Draws a rectangle.
+ * @brief Draws a rectangle outline.
  * 
  * @param rendering_context    The rendering context.
  * @param x                    Top left x coordinate.
@@ -41,28 +22,10 @@ void canvas_draw_line(canvas_rendering_context_t* rendering_context, int16_t x1,
  * @param width                Width of the rectangle.
  * @param height               Height of the rectangle.
 */
-void canvas_draw_rectangle(
+void canvas_stroke_rectangle(
     canvas_rendering_context_t* rendering_context,
     int16_t x, int16_t y,
     uint16_t width, uint16_t height
-);
-
-/** 
- * @brief Draws an elliptical arc.
- * 
- * @param rendering_context    The rendering context used.
- * @param x                    Top left x coordinate of the rectangle surrounding the ellipse.
- * @param y                    Top left y coordinate of the rectangle surrounding the ellipse.
- * @param width                Width of the rectangle surrounding the ellipse.
- * @param height               Height of the rectangle surrounding the ellipse.
- * @param angle1               Starting angle of the arc.
- * @param angle2               Ending angle of the arc.
-*/
-void canvas_draw_arc(
-    canvas_rendering_context_t* rendering_context,
-    int16_t x, int16_t y,
-    uint16_t width, uint16_t height,
-    int16_t angle1, int16_t angle2
 );
 
 /** 
@@ -77,24 +40,6 @@ void canvas_fill_rectangle(
     canvas_rendering_context_t* rendering_context,
     int16_t x, int16_t y,
     uint16_t width, uint16_t height
-);
-
-/**
- * @brief Draws a filled ellipse.
- * 
- * @param rendering_context The rendering context used.
- * @param x                 Top left x coordinate of the rectangle surrounding the ellipse.
- * @param y                 Top left y coordinate of the rectangle surrounding the ellipse.
- * @param width             Width of the rectangle surrounding the ellipse.
- * @param height            Height of the rectangle surrounding the ellipse.
- * @param angle1            Starting angle of the arc.
- * @param angle2            Ending angle of the arc.
- */
-void canvas_fill_arc(
-    canvas_rendering_context_t* rendering_context,
-    int16_t x, int16_t y,
-    uint16_t width, uint16_t height,
-    int16_t angle1, int16_t angle2
 );
 
 /**
--- src/canvas.c
+++ src/canvas.c
@@ -5,25 +5,7 @@ void canvas_init (canvas_rendering_context_t* rendering_context)
     xcbcanvas_init_xcb(rendering_context);
 }
 
-void canvas_draw_point (canvas_rendering_context_t* rendering_context, int16_t x, int16_t y)
-{
-    xcb_point_t point = {x, y};
-    xcb_poly_point(rendering_context->c, XCB_COORD_MODE_ORIGIN, rendering_context->win, rendering_context->foreground,
-        1, &point);
-}
-
-void canvas_draw_line (canvas_rendering_context_t* rendering_context, int16_t x1, int16_t y1, int16_t x2, int16_t y2)
-{
-    xcb_segment_t segment = {
-        .x1 = x1,
-        .y1 = y1,
-        .x2 = x2,
-        .y2 = y2
-    };
-    xcb_poly_segment(rendering_context->c, rendering_context->win, rendering_context->foreground, 1, &segment);
-}
-
-void canvas_draw_rectangle(
+void canvas_stroke_rectangle(
     canvas_rendering_context_t* rendering_context,
     int16_t x, int16_t y,
     uint16_t width, uint16_t height
@@ -36,24 +18,6 @@ void canvas_draw_rectangle(
         .height = height
     };
     xcb_poly_rectangle(rendering_context->c, rendering_context->win, rendering_context->foreground, 1, &rectangle);
-}
-
-void canvas_draw_arc(
-    canvas_rendering_context_t* rendering_context,
-    int16_t x, int16_t y,
-    uint16_t width, uint16_t height,
-    int16_t angle1, int16_t angle2
-    )
-{
-    xcb_arc_t arc = {
-        .x = x,
-        .y = y,
-        .width = width,
-        .height = height,
-        .angle1 = angle1,
-        .angle2 = angle2
-    };
-    xcb_poly_arc(rendering_context->c, rendering_context->win, rendering_context->foreground, 1, &arc);
 }
 
 void canvas_fill_rectangle(
@@ -69,24 +33,6 @@ void canvas_fill_rectangle(
         .height = height
     };
     xcb_poly_fill_rectangle(rendering_context->c, rendering_context->win, rendering_context->foreground, 1, &rectangle);
-}
-
-void canvas_fill_arc(
-    canvas_rendering_context_t* rendering_context,
-    int16_t x, int16_t y,
-    uint16_t width, uint16_t height,
-    int16_t angle1, int16_t angle2
-    )
-{
-    xcb_arc_t arc = {
-        .x = x,
-        .y = y,
-        .width = width,
-        .height = height,
-        .angle1 = angle1,
-        .angle2 = angle2
-    };
-    xcb_poly_fill_arc(rendering_context->c, rendering_context->win, rendering_context->foreground, 1, &arc);
 }
 
 void canvas_draw_text(
