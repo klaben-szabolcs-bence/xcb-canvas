3567d2403411af8abdba8300c59434316005e0ad
Tudi20 <tudorkae@gmail.com>
2022-02-16 08:47:23 +0100

    Finish wrapping xcb-canvas.c

--- tests/xcb-canvas-wrap.c
+++ tests/xcb-canvas-wrap.c
@@ -11,10 +11,27 @@
  * functions, it could do so by calling the special symbol __real_xcbcanvas_init_xcb().
  */
 
- // TODO: This is a stub, it should be implemented.
 int __wrap_xcbcanvas_init_xcb(__wrap_canvas_rendering_context_t* rendering_context)
 {
+    rendering_context->window_size.width = 800;
+    rendering_context->window_size.height = 600;
+    rendering_context->background = 0;
+    rendering_context->foreground = 1;
+    __wrap_xcbcanvas_handle_events(rendering_context);
     return 0;
 }
 
-// TODO: Create tests for the setters and getters.
+void __wrap_xcbcanvas_set_window_size(__wrap_canvas_rendering_context_t* rendering_context, int new_width, int new_height)
+{
+    rendering_context->window_size.width = new_width;
+    rendering_context->window_size.height = new_height;
+}
+
+xcbcanvas_size_t __wrap_xcbcanvas_get_window_size(__wrap_canvas_rendering_context_t* rendering_context) {
+    return rendering_context->window_size;
+}
+
+void __wrap_xcbcanvas_handle_events(__wrap_canvas_rendering_context_t* rendering_context)
+{
+    rendering_context->draw_function();
+}
