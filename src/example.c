4804ce1d8562e6d6121718283fa84e32f511529c
Kláben Szabolcs Bence <szabolcs.bence@klaben.hu>
2023-10-20 16:36:07 +0200

    Add to exmaple the chaging of color and setting line width

--- src/example.c
+++ src/example.c
@@ -9,9 +9,14 @@ int main()
         printf("Error initialising canvas\n");
         return 1;
     }
+
+    free(rendering_context.canvas);
 }
 
 void draw()
 {
     canvas_fill_rectangle(&rendering_context, 0, 0, 100, 100);
+    canvas_set_line_width(&rendering_context, 20);
+    canvas_set_color(&rendering_context, 255, 0, 0);
+    canvas_stroke_rectangle(&rendering_context, 50, 50, 100, 100);
 }
--- tests/canvas-tests.c
+++ tests/canvas-tests.c
@@ -4,6 +4,11 @@
 
 void canvas_init_test(void** state)
 {
+    ((struct state*)state)->rendering_context->draw_function = canvas_draw;
     int rc = canvas_init(((struct state*)state)->rendering_context);
     assert_return_code(rc, 0);
+}
+
+void canvas_draw(void** state)
+{
 }
