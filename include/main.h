ba4a99df8a4f44cd54637c5539618af8df58b203
Kláben Szabolcs Bence <szabolcs.bence@klaben.hu>
2023-10-20 16:35:59 +0200

    Take out event handling and disable dbg msgs

--- include/main.h
+++ include/main.h
@@ -12,4 +12,6 @@ void print_modifiers (uint32_t mask);
 /* Program entry-point, when executed */
 int main ();
 
+/* Handle the event loop of the main program */
+void handle_events (xcb_connection_t *c);
 #endif /* MAIN_H */
