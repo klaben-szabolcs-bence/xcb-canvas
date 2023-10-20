4d10a4e8ceca1f74099e5707ab5f9fa8a5927098
Kláben Szabolcs Bence <szabolcs.bence@klaben.hu>
2023-10-20 16:36:08 +0200

    Finish basic Path support

--- src/example.c
+++ src/example.c
@@ -4,13 +4,14 @@ int main()
 {
     canvas_set_draw_function(&rendering_context, draw);
 
-    if (!canvas_init(&rendering_context))
-    {
+    if (canvas_init(&rendering_context) != 0) {
         printf("Error initialising canvas\n");
         return 1;
     }
 
     canvas_dealloc(&rendering_context);
+
+    return 0;
 }
 
 void draw()
@@ -19,4 +20,14 @@ void draw()
     canvas_set_line_width(&rendering_context, 20);
     canvas_set_color(&rendering_context, 255, 0, 0);
     canvas_stroke_rectangle(&rendering_context, 50, 50, 100, 100);
+
+    /* Draw a blue triangle */
+    canvas_set_color(&rendering_context, 0, 0, 255);
+    canvas_set_line_width(&rendering_context, 5);
+    canvas_begin_path(&rendering_context);
+    canvas_move_to(&rendering_context, 200, 200);
+    canvas_line_to(&rendering_context, 300, 200);
+    canvas_line_to(&rendering_context, 300, 300);
+    canvas_close_path(&rendering_context);
+    canvas_fill(&rendering_context);
 }
